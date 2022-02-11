c5287ce994b2eb10a585422a8ad96622c8d28a75
lcsdovalle <contatolucasthomas@gmail.com>
2022-02-11 16:06:44 -0300

    Deploying test

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ gcloud iam service-accounts keys create dbadmin.json --iam-account=$SSA
 gcloud projects add-iam-policy-binding $PROJECT_ID \
 --member serviceAccount:$SSA \
 --role=roles/cloudsql.admin
-gcloud functions deploy cleanUpDB --runtime python39 --trigger-http --allow-unauthenticated
+gcloud functions deploy cleanUpDB --runtime python39 --set-env-vars GCP_PROJECT=$PROJECT_ID --trigger-http --allow-unauthenticated
 
